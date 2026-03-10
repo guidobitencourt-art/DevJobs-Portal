022aaebced925dba87defb2dbda6744ebef2f2ba
Ana <arperezch13@alumnos.cei.es>
2026-03-10 20:11:34 +0100

    Fix import statement for TarjetaEmpleo component and update route element

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import Dev2 from './Components/DevJobs/Dev2'
-import Dev3 from './Components/DevJobs/TarjetaEmpleo'
+import TarjetaEmpleo from './Components/DevJobs/TarjetaEmpleo'
 
 
 function App() {
@@ -12,7 +12,17 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Dev2 />} />
-        <Route path="/ofertas" element={<Dev3 />} />
+        <Route
+          path="/ofertas"
+          element={
+            <TarjetaEmpleo
+              id={1}
+              titulo="Desarrollador Frontend"
+              modalidad="remoto"
+              esUrgente={true}
+            />
+          }
+        />
       </Routes>
     </div>
   )
