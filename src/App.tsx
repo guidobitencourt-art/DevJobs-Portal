6110571baad2fb71a905a560440fd46f08f108c1
Ana <arperezch13@alumnos.cei.es>
2026-03-10 20:09:42 +0100

    Add react-router-dom for routing and implement job listing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Dev2 from './Components/DevJobs/Dev2'
+import Dev3 from './Components/DevJobs/TarjetaEmpleo'
 
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <nav>
+        <Link to="/">Inicio</Link> | <Link to="/ofertas">Ofertas</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Dev2 />} />
+        <Route path="/ofertas" element={<Dev3 />} />
+      </Routes>
+    </div>
   )
 }
 
